d3aefdf2cddd6c63c8a256736b277c99092f9388
Cole Grim <cg@netfla.me>
2024-06-30 12:30:44 -0700

    updated script

--- scripts/remote_update.sh
+++ scripts/remote_update.sh
@@ -3,7 +3,7 @@ echo Starting update at $(date -u) >> /tmp/update_log
 
 cd ~/workspace
 podman stop webpage
-podman build -t webpage
+podman build -t webpage .
 podman run -t webpage --name webpage
 
 echo Completed update at $(date -u) >> /tmp/update_log
